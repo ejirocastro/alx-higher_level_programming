c9d087a8d212da0cc2df334ec1ccb39c1ef3d5e1
ejirocastro <awenayericastrop@gmail.com>
2024-03-26 19:12:59 -0700

    TASK 7

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- inserts a new row in the table first_table in MySQL server.
-INSERT INTO first_table VALUES (id = 89, name = Best of School);
+INSERT INTO first_table (id, name) VALUES (89, "Best of School");
