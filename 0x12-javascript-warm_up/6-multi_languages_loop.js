9623fa3f106fdfcb54728392f8a631879410a633
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 15:44:35 -0700

    TASK 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a script that prints 3 lines:ike 1-multi_languages.js */
+const myVarray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myVarray.length; i++) {
+  console.log(myVarray[i]);
+}
