24323c6c30dc13f96ddfecaa514d62cbb3901455
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 10:22:40 -0700

    TAS1 CORT

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log(myVar[0] + '\n', myVar[1] + '\n', myVar[2]);
+console.log(myVar[0] + '\n',myVar[1] + '\n',myVar[2]);
