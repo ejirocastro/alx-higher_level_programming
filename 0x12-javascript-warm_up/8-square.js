ba14f9f599626939a22cd4e388a6e84daf1b8135
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 16:38:42 -0700

    TASK 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* a script that prints a square */
+const size = process.argv[2];
+const mySize = parseInt(size);
+const x = 'x';
+
+if (isNaN(mySize)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < mySize; i++) {
+    console.log(x.repeat(mySize));
+  }
+}
