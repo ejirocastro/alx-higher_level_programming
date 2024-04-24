8353f43e6a1898907bc7a685805313255ca1ce71
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 11:44:33 -0700

    TASK 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* script that prints argument passed */
+const argsL = process.argv.length;
+if (argsL === 2) {
+	console.log('No argument');
+} else if (argsL === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
