0c3fbc60e68557025bd7cdf0b8d9c5f7f5669cfa
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 11:14:35 -0700

    TASK1C

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-/*a script that prints 3 lines*/
-console.log('C is fun\nPython is coo\nJavaScript is amazing');
+/* a script that prints 3 lines */
+const lineFun = 'C is Fun';
+const lineCool = 'Python is cool';
+const lineAmaze = 'JavaScript is amazing';
+console.log(lineFun);
+console.log(lineCool);
+console.log(lineAmaze);
