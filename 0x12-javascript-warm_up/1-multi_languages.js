acbbbcb3590d4a28213a257d7bbd98701b8257db
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 10:51:05 -0700

    CT1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const line1 = "C is fun";
-const line2 = "Python is cool";
-const line3 = "JavaScript is amazing";
-console.log(line1);
-console.log(line2);
-console.log(line3);
+const lineFun = "C is fun";
+const lineCool = "Python is cool";
+const lineAmaze = "JavaScript is amazing";
+console.log(lineFun);
+console.log(lineCool);
+console.log(lineAmaze);
