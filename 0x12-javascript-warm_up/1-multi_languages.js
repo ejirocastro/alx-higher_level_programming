277fcbd29c9858f37380d2e3adb5527c1046908c
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 11:21:10 -0700

    CTASK1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 /* a script that prints 3 lines */
-
-const lineFun = 'C is Fun';
+const lineFun = 'C is fun';
 const lineCool = 'Python is cool';
 const lineAma = 'JavaScript is amazing';
 console.log(lineFun);
 console.log(lineCool);
-console.log(lineAmaze);
+console.log(lineAma);
