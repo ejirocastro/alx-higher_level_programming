fde19db638fa4fdf62b114bd41ef405695e700d5
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 11:16:07 -0700

    Task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 /* a script that prints 3 lines */
+
 const lineFun = 'C is Fun';
 const lineCool = 'Python is cool';
-const lineAmaze = 'JavaScript is amazing';
+const lineAma = 'JavaScript is amazing';
 console.log(lineFun);
 console.log(lineCool);
 console.log(lineAmaze);
