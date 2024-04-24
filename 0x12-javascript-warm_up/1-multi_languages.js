f81a09b0523a59b885f28be2815c777d114f829c
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 10:40:23 -0700

    CTASK

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
 
 const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
-console.log(myVar[0] + '\n',myVar[1] + '\n',myVar[2]);
+for (const element of myVar);
+{
+	console.log(element);
+}
