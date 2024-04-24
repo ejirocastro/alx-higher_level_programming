5a44db6e3cb042241b81adee8f0561a2401ec0b2
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 11:07:26 -0700

    TASK 1 CORT

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-
-const lineFun = "C is fun";
-const lineCool = "Python is cool";
-const lineAmaze = "JavaScript is amazing";
-console.log(lineFun);
-console.log(lineCool);
-console.log(lineAmaze);
+/* a script that prints 3 lines */
+console.log('C is fun\nPython is coo\nJavaScript is amazing');
