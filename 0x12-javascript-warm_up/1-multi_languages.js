95dc1eb03a41a351a712d7c2757fa3779f999002
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 10:44:52 -0700

    TASK CORRECTION 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-const myVar = ["C is fun", "Python is cool", "JavaScript is amazing"];
-for (const element of myVar);
-{
-	console.log(element);
-}
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+console.log(line1);
+console.log(line2);
+console.log(line3);
