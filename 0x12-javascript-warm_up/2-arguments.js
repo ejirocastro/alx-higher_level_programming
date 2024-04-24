234a825bbbebf8408758e52fc2ffde36635155e4
eirocastro <awenayericastrop@ggmail.com>
2024-04-24 12:01:42 -0700

    TASK2CRT

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /* script that prints argument passed */
 const argsL = process.argv.length;
 if (argsL === 2) {
-	console.log('No argument');
+  console.log('No argument');
 } else if (argsL === 3) {
-	console.log('Argument found');
+  console.log('Argument found');
 } else {
-	console.log('Arguments found');
+  console.log('Arguments found');
 }
