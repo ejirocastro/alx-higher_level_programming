5e38672909b2b3cfe9f2f5a417db702b4bee028d
ejirocastro <awenayericastrop@gmail.com>
2024-05-09 14:27:52 -0700

    TASK3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send an OPTIONS request to get supported methods
-curl -s -X OPTIONS -i "$1" | grep "Allow:" | awk '{print $2}'
+#get the methods and in the head request
+curl -sH "X-School-User-Id: 98" "$1"
