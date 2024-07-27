a3644c6fa6cf21f9063893fc113d2ed7925c18d5
ejirocastro <awenayericastrop@gmail.com>
2024-07-27 17:35:25 +0100

    Task 5 correction

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,57 @@
 #!/usr/bin/python3
-Square = __import__('5-square').Square
 
-my_square = Square(3)
-my_square.my_print()
+"""
+1. Square with size validation
+    Contains a 'Square' class that defines a square which accepts
+    a size at instantiation and provides a function for calculating
+    it's area and another function for printing the square.
+"""
 
-print("--")
 
-my_square.size = 10
-my_square.my_print()
+class Square:
+    """
+    An square class that accepts a size
+    """
 
-print("--")
+    def __init__(self, size=0):
+        """
+        initialize class with size
+        """
+        self.size = size
 
-my_square.size = 0
-my_square.my_print()
+    @property
+    def size(self):
+        """
+        get size of square
+        """
+        return self.__size
 
-print("--")
+    @size.setter
+    def size(self, value):
+        """
+        set size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        prints in stdout the square
+        with the character '#'
+        """
+        if self.size == 0:
+            print()
+            return
+        for _ in range(self.size):
+            for _ in range(self.size):
+                print("#", end="")
+            print()
